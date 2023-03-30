92c5ff6a4ecac9085e92414a65ac14a6eafb6d22
Bhushan <bhapkarbhushan@gmail.com>
2023-03-30 13:31:01 +0530

    undo react setup

---
+++ files/captcha-validation.js
@@ -0,0 +1,30 @@
+//captcha validation start
+function refreshCaptcha()
+{
+	var img = document.images['captchaimg'];
+	img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
+}
+
+
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+} 
+// captcha validation end
